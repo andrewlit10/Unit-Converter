43836ee0983b7fa149cefff93b1be8a36c83e33f
Andrew Litten <andrew.litten@gmail.com>
2024-06-18 19:13:12 -0700

    added html and css structure and styling

---
+++ index.js
@@ -0,0 +1,10 @@
+/*
+1 meter = 3.281 feet
+1 liter = 0.264 gallon
+1 kilogram = 2.204 pound
+*/
+let inputEl = document.querySelector(".input");
+let submitBtn = document.querySelector(".input-btn");
+let lengthEl = document.querySelector(".length-p");
+let volumeEl = document.querySelector(".volume-p");
+let massEl = document.querySelector(".mass-p");
