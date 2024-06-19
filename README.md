e2fcbb1d524f667eeefb9c74d1c5ff00927b2666
Andrew Litten <andrew.litten@gmail.com>
2024-06-18 20:50:14 -0700

    I added logic to javascript file
    I also required input to be a number and that it is above 0, otherwise it alerts browser to enter valid number.

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Unit Converter
+
  Unit Converter Project
+
+# Project idea from Scrimba
+
+https://projects.scrimba.com/unit-converter
+
+# Design 
+
+ https://www.figma.com/design/7zNr8t65j7CMPZU1UWPiHX/Unit-Conversion-(Copy)?node-id=0-13&t=ihW2t9U3WRQXWHed-0
+
+# Completed by Andrew Litten
--- index.js
+++ index.js
@@ -1,10 +1,25 @@
-/*
-1 meter = 3.281 feet
-1 liter = 0.264 gallon
-1 kilogram = 2.204 pound
-*/
 let inputEl = document.querySelector(".input");
 let submitBtn = document.querySelector(".input-btn");
 let lengthEl = document.querySelector(".length-p");
 let volumeEl = document.querySelector(".volume-p");
 let massEl = document.querySelector(".mass-p");
+
+
+const meterToFeet =  3.281
+const literToGallon =  0.264
+const kiloToPound =  2.204
+
+
+submitBtn.addEventListener("click", function(){
+    let baseValue = parseFloat(inputEl.value);
+    if (isNaN(baseValue) || baseValue === 0) {
+        alert("Please enter a valid number!");
+    } else {
+    lengthEl.textContent = `${baseValue} meters = ${(baseValue * meterToFeet).toFixed(3)} feet | ${baseValue} feet = ${(baseValue / meterToFeet).toFixed(3)} meters`;
+    volumeEl.textContent = `${baseValue} liters = ${(baseValue * literToGallon).toFixed(3)} gallons | ${baseValue} gallons = ${(baseValue / literToGallon).toFixed(3)} liters`;
+    massEl.textContent = `${baseValue} kilometers = ${(baseValue * kiloToPound).toFixed(3)} pounds | ${baseValue} pounds = ${(baseValue / kiloToPound).toFixed(3)} kilometers`;
+    
+    }
+})
+
+
